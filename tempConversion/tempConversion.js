ce0311ddc28cdd084b54c3c3d0a9fd1d6d943203
xnlou <xnlouey@gmail.com>
2019-02-04 23:52:37 -0600

    completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (far) {
+  let c_temp = Math.round((far - 32) * 5 / 9 * 10) / 10;
+  return c_temp;
 }
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let f_temp = Math.round(((cel * 9 / 5 ) + 32 )* 10) / 10;
+  return f_temp;
 }
 
 module.exports = {
