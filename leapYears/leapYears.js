c1a4d29fc01dd0f2b515ab8e9470b80b82d258c5
xnlou <xnlouey@gmail.com>
2019-02-04 23:10:43 -0600

    completed leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
-}
-
+const leapYears = function(year) {
+    let is_leap = '';
+    // Check if input is number
+    if (!(typeof(year) == 'number')){
+      return "ERROR, enter a numeric year";
+    }
+    // Check for century years
+    if ((year % 100 == 0) && !(year % 400 == 0)){
+      is_leap = false;
+    } else if (year % 4 == 0){
+      is_leap = true;
+    } else is_leap = false;
+    return is_leap;
+  }
 module.exports = leapYears
