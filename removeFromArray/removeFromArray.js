a79dc474b5b9a49284a83d94e9ad1de5e86256b0
xnlou <xnlouey@gmail.com>
2019-02-04 18:48:20 -0600

    helloWorld reverseString repeatString removeString removeFromArray complete

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,8 +1,14 @@
-const removeFromArray = function(array,rem_sp) {
-    let rem_array = [];
-
-    rem_array = array.splice(rem_sp -1 ,1,)
-   
-    return array;
+const removeFromArray = function(array) {
+    // Loop for number of arguments
+        for (i = 1; i < arguments.length; i++) {
+            // Index of gives location of argument within array
+            var argRemove = array.indexOf(arguments[i]);
+            // Removing based on index
+            if (array.includes(arguments[i])) {
+                   array.splice(argRemove, 1);
+               }
+          }
+          return array;
+    
 }
 module.exports = removeFromArray
